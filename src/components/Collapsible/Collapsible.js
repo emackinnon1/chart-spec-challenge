6423674201858210da47a1fd00ef3443eeffa951
Elliot Mackinnon <elliotmackinnon@techtonics-MacBook-Pro.local>
2020-11-11 09:28:49 -0700

    Create collapsible component

---
+++ src/components/Collapsible/Collapsible.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Collapsible = (props) => {
+  return (
+    <div>
+      <p>Attire: {props?.attire}</p>
+      <p>{props?.hours}</p>
+      <p>Latitude: {props?.lat}</p>
+      <p>Longitude: {props?.long}</p>
+      <p>{props?.tags}</p>
+    </div>
+  );
+};
+
+export default Collapsible;
