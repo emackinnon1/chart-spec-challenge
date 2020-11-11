07917e12021b6ca58654cc26b3d27972dbe721bc
emackinnon1 <emackinnon100390@gmail.com>
2020-11-10 20:35:44 -0700

    Create components directory and basic file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, {
+    useState,
+    useEffect
+} from "react";
+import './App.css';
+
+const App = () => {
+    const [restaurantsList, setRestaurantsList] = useState([]);
+
+    return ( <
+        div className = "App" >
+        Hello <
+        /div>
+    );
+}
+
+export default App;
